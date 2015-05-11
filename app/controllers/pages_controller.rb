ef18355ec810eea0d91d5e44dc35f4a8805b8da1
Kate Barrera <kate@launchlabs.ph>
2015-05-11 19:04:19 +0800

    Add condition on is_draft and is_archived

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
   #before_filter :authenticate_user!
 
 	def home
-    @blogs = Blog.order("created_at").paginate(:page => params[:page], :per_page => 5)
-	end
+    @blogs = Blog.where(:is_draft => false, :is_archived => false).order("created_at").paginate(:page => params[:page], :per_page => 5)
+  end
 end

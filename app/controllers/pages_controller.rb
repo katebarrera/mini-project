ecec76c3505585b22bc28b289081c8d0b76c998f
Kate Barrera <kate@launchlabs.ph>
2015-05-11 18:14:59 +0800

    displayed all live blogs and paginate them in the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
 
+  #before_filter :authenticate_user!
+
 	def home
+    @blogs = Blog.order("created_at").paginate(:page => params[:page], :per_page => 5)
 	end
 end

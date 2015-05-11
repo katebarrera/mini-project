b524e9495df161585342e6175b75537f9796f3dc
Kate Barrera <kate@launchlabs.ph>
2015-05-11 13:15:09 +0800

    Coded the actions for index, show, create and new for blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,19 +1,34 @@
 class BlogsController < ApplicationController
 
 	def index
+		@blog = Blog.all
 	end
 
 	def show
+		@blog = Blog.find(params[:id])
 	end
 
 	def edit
 	end
 
 	def create
-	end
+		@blog = Blog.new(blog_params)
 
+		if @blog.save
+			flash[:success] = "Blog created!"
+			redirect_to blogs_path
+		else
+			render 'pages/home'
+		end
+	end
 
 	def new
+		@blog = current_user.blogs.build
 	end
-	
+
+	private
+
+		def blog_params
+			params.require(:blog).permit(:title, :caption, :description)
+		end
 end

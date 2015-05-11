3f28df67d9136478ea8d8471e9c92e676fe29ac2
Kate Barrera <kate@launchlabs.ph>
2015-05-11 13:19:03 +0800

    Configured routes for the show, create and new blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :blogs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'pages#home'
+   get 'new_blog' =>'blogs#new'
+   get 'show_blog' => 'blogs#show'
+   post 'blogs' => 'blogs#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

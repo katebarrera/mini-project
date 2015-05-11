839088d3016452117b8ec30ef9501d7c8a9a2e55
Kate Barrera <kate@launchlabs.ph>
2015-05-11 20:07:04 +0800

    migrated db without id column

--- db/migrate/20150511113241_create_categories.rb
+++ db/migrate/20150511113241_create_categories.rb
@@ -1,7 +1,6 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.string :id
       t.string :name
 
       t.timestamps null: false
--- db/migrate/20150511113440_create_blogs_categories.rb
+++ db/migrate/20150511113440_create_blogs_categories.rb
@@ -1,7 +1,6 @@
 class CreateBlogsCategories < ActiveRecord::Migration
   def change
     create_table :blogs_categories do |t|
-      t.integer :id
       t.integer :category_id
       t.integer :blog_id
 

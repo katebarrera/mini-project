2271b8dce3d7c307f10bacac9abd9e7e9669c0b1
Kate Barrera <kate@launchlabs.ph>
2015-05-12 19:15:09 +0800

    added validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < ActiveRecord::Base
 	belongs_to :user
   has_and_belongs_to_many :categories
+
+  validates :title, presence: true
+  validates :caption, presence: true
+  validates :description, presence: true
 end

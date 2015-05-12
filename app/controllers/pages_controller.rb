328a3d2a6b0a1919f9b3e6482cff2ed8d122a5c3
Kate Barrera <kate@launchlabs.ph>
2015-05-12 19:14:57 +0800

    viewed the list of blogs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     if user_signed_in?
       redirect_to blogs_path
     else
-      @blogs = Blog.where(:is_draft => false, :is_archived => false).order("created_at").paginate(:page => params[:page], :per_page => 5)
-    end
+      @blogs = Blog.where(:is_draft => false, :is_archived => false).order("created_at desc").paginate(:page => params[:page], :per_page => 5)
+   end
   end
 end
